3bd3b39731c3f1dd6bdc69ec708143eb77b2081b
Thomas Nelson <thnlsn@gmail.com>
2020-05-26 12:54:48 -0700

    edited the head of the first-posts page using the custom built-in Head component from NextJS

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,5 +1,21 @@
-// Can have any name (FirstPost), but msut be exported as a default export
+import Head from 'next/head';
 
+// TO be used to create a link back to the homepage
+import Link from 'next/link';
+
+// Can have any name (FirstPost), but msut be exported as a default export
 export default function FirstPost() {
-  return <h1>First Post</h1>;
+  return (
+    <>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <h1>First Post</h1>
+      <h2>
+        <Link href='/'>
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </>
+  );
 }
